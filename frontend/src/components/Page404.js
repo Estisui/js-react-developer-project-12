80c7b89d09045b6d3ad9a51e86863139ee19c557
Stepan Sukhachev <estisui@protonmail.ch>
2023-09-13 12:31:34 +0300

    feat: add bootstrap + 404 page styles

--- frontend/src/components/Page404.js
+++ frontend/src/components/Page404.js
@@ -1,7 +1,7 @@
 const Page404 = () => {
   return(
-    <div className="404">
-      <p>Error</p>
+    <div className="d-flex align-items-center justify-content-center vh-100 bg-primary">
+        <h1 className="display-1 fw-bold text-white">404</h1>
     </div>
   );
 };
