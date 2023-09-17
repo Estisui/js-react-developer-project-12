d254e69fd0d77cc443cb9a8e43fa86fcc66dbf4b
Stepan Sukhachev <estisui@protonmail.ch>
2023-09-17 12:47:30 +0300

    feat: add login form + change theme to dark

--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -1,7 +1,71 @@
+import {
+  useFormik,
+} from 'formik';
+
+const SignupForm = () => {
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      password: "",
+    },
+    onSubmit: (values) => {
+      console.log(JSON.stringify(values, null, 2));
+    },
+  });
+  return (
+    <form className='col-12 mt-3 mt-mb-0' onSubmit={formik.handleSubmit}>
+      <h1 className="text-center mb-4">Войти</h1>
+      <div className='form-floating mb-3'>
+        <input
+          name="username"
+          autoComplete="username"
+          required
+          placeholder="Ваш ник"
+          id="username"
+          className="form-control"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+        />
+        <label htmlFor="username">Ваш ник</label>
+      </div>
+      <div className='form-floating mb-4'>
+        <input
+          name="password"
+          autoComplete="current-password"
+          required
+          placeholder="Пароль"
+          type='password'
+          id='password'
+          className='form-control'
+          onChange={formik.handleChange}
+          value={formik.values.password}
+        />
+        <label className="form-label" htmlFor="password">Пароль</label>
+      </div>
+      <button type="submit" className="w-100 mb-3 btn btn-outline-primary">Войти</button>
+    </form>
+  );
+};
+
 const LoginPage = () => {
   return(
-    <div className="login">
-      <p>Login</p>
+    <div className="login d-flex flex-column h-100 text-light">
+      <nav className="shadow-sm navbar navbar-expand-lg navbar-light">
+        <div className="container">
+          <a className="navbar-brand text-light" href="/">Chat</a>
+        </div>
+      </nav>
+      <div className='container-fluid h-100'>
+        <div className='row justify-content-center align-content-center h-100'>
+          <div className='col-12 col-md-8 col-xxl-6'>
+            <div className='card border-light shadow-sm'>
+              <div className='card-body row p-5'>
+                <SignupForm />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- frontend/src/components/Page404.js
+++ frontend/src/components/Page404.js
@@ -1,6 +1,6 @@
 const Page404 = () => {
   return(
-    <div className="d-flex align-items-center justify-content-center vh-100 bg-primary">
+    <div className="d-flex align-items-center justify-content-center vh-100">
         <h1 className="display-1 fw-bold text-white">404</h1>
     </div>
   );
